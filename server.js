51b2e88fc659145c4bdc30553c05dc34d1d13f04
Abraham Hsu <smokiebacon@gmail.com>
2019-01-22 16:35:22 -0800

    added middleware

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
